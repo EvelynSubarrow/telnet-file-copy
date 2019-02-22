10fe32b06cef063caa043e2491a365d3729f38c4
Evelyn Snow <git@evelyn.moe>
2019-02-22 12:22:20 +0000

    Hash verification, verbose mode, fix preliminary deletion

--- telnet-file-copy.py
+++ telnet-file-copy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import socket, select, sys, time, os, argparse
+import socket, select, sys, time, os, argparse, hashlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument('host')
@@ -8,7 +8,10 @@ parser.add_argument("port", type=int)
 
 parser.add_argument('file_here', type=argparse.FileType("rb"))
 parser.add_argument('file_there')
+parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
 parser.add_argument("-b", "--block-size", type=int, default=200, help="Number of bytes transferred each line")
+parser.add_argument("-R", "--no_removal", action="store_true", help="Do not attempt to remove file first")
+parser.add_argument("-H", "--no-hash-verification", action="store_true", default=False, help="Do not attempt to verify file hash")
 parser.add_argument("--username", help="username")
 parser.add_argument("--password", help="password")
 args = parser.parse_args()
@@ -27,14 +30,21 @@ with args.file_here as f, socket.create_connection((args.host, args.port)) as s:
     file_size = f.tell()
     f.seek(0)
 
-    s.send("rm {}".format(args.file_there).encode("utf8"))
+    if not args.no_removal:
+        s.send("rm {}\n".format(args.file_there).encode("utf8"))
     s.send(b"echo READY\n")
 
+    hash = hashlib.new("md5")
+
     data = f.read(args.block_size)
+    hash.update(data)
+
     buffer_read = b""
     i = 0
+
+    done = False
     start_time = time.time()
-    while data:
+    while not done:
         if select.select([s], [], [], 10)[0]:
             recv_data = s.recv(4096)
             lines = (buffer_read+recv_data).split(b"\n")
@@ -45,7 +55,9 @@ with args.file_here as f, socket.create_connection((args.host, args.port)) as s:
 
             for line in lines:
                 line = line.rstrip(b"\r")
-                if line==b"READY":
+                if args.verbose:
+                    print(line)
+                if line==b"READY" and data:
                     s.send("echo -ne '{}' >{} {} ; echo READY\n".format(
                             ''.join([r"\x%02X" % a for a in data]),
                             ">"*bool(i),
@@ -54,8 +66,23 @@ with args.file_here as f, socket.create_connection((args.host, args.port)) as s:
 
                     print("\r{}/{} {}s".format(i*args.block_size + len(data), file_size, int(time.time()-start_time)), end='')
                     data = f.read(args.block_size)
+                    hash.update(data)
                     i += 1
+                elif line==b"READY":
+                    if not args.no_hash_verification:
+                        s.send("echo HASH `md5sum {}`\n".format(args.file_there).encode("utf8"))
+                    else:
+                        done=True
+                elif line.startswith(b"HASH "):
+                    remote_hash = line.split(b" ")[1].decode("utf8")
+                    local_hash = hash.hexdigest()
+                    print()
+                    print("Local  hash {}".format(local_hash))
+                    print("Remote hash {}".format(remote_hash))
+                    print("Hashes {}".format("match"*(local_hash==remote_hash) or "do not match"))
+                    done = True
         else:
+            print()
             print("Read timed out")
             exit()
     print()
