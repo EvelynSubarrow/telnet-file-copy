5b3c9f7969bfed7044a293c28c775fb446a6cff9
Evelyn Snow <git@evelyn.moe>
2019-02-22 16:32:09 +0000

    Add base64 support

--- telnet-file-copy.py
+++ telnet-file-copy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import socket, select, sys, time, os, argparse, hashlib
+import socket, select, sys, time, os, argparse, hashlib, base64
 
 parser = argparse.ArgumentParser()
 parser.add_argument('host')
@@ -15,6 +15,9 @@ parser.add_argument("-H", "--no-hash-verification", action="store_true", default
 parser.add_argument("--username", help="username")
 parser.add_argument("--password", help="password")
 args = parser.parse_args()
+
+MECHANISM_ECHO   = 1
+MECHANISM_BASE64 = 2
 
 with args.file_here as f, socket.create_connection((args.host, args.port)) as s:
     s.setblocking(0)
@@ -35,15 +38,19 @@ with args.file_here as f, socket.create_connection((args.host, args.port)) as s:
     s.send(b"echo | sha256sum && echo HASHASH sha256\n")
     s.send(b"echo | sha512sum && echo HASHASH sha512\n")
 
+    s.send(b"echo | base64 && echo HASBASE64\n")
+
     if not args.no_removal:
         s.send("rm {}\n".format(args.file_there).encode("utf8"))
     s.send(b"echo READY\n")
 
     hash = None
-
     data = 1
+
     buffer_read = b""
     i = 0
+
+    mechanism = MECHANISM_ECHO
 
     done = False
     start_time = time.time()
@@ -64,11 +71,21 @@ with args.file_here as f, socket.create_connection((args.host, args.port)) as s:
                     data = f.read(args.block_size)
                     hash.update(data)
                     if data:
-                        s.send("echo -ne '{}' >{} {} ; echo READY\n".format(
-                                ''.join([r"\x%02X" % a for a in data]),
-                                ">"*bool(i),
-                                args.file_there
-                            ).encode("utf8"))
+                        if mechanism == MECHANISM_ECHO:
+                            s.send("echo -ne '{}' >{} {} ; echo READY\n".format(
+                                    ''.join([r"\x%02X" % a for a in data]),
+                                    ">"*bool(i),
+                                    args.file_there
+                                ).encode("utf8"))
+                        elif mechanism == MECHANISM_BASE64:
+                            s.send("echo '{}' | base64 -d >{} {} ; echo READY\n".format(
+                                    base64.b64encode(data),
+                                    ">"*bool(i),
+                                    args.file_there
+                                ).encode("utf8"))
+                        else:
+                            print("Unsupported transfer mechanism")
+                            exit(os.EX_DATAERR)
 
                         print("\r{}/{} {}s".format(i*args.block_size + len(data), file_size, int(time.time()-start_time)), end='')
                         i += 1
@@ -89,8 +106,12 @@ with args.file_here as f, socket.create_connection((args.host, args.port)) as s:
                     else:
                         exit(os.EX_PROTOCOL)
                 elif line.startswith(b"HASHASH"):
-                    hash = hashlib.new(line.split(b" ")[1].decode("utf8"))
-                    print()
+                    hash_algo = line.split(b" ")[1].decode("utf8")
+                    print("Remote host supports {} hashing".format(hash_algo))
+                    hash = hashlib.new(hash_algo)
+                elif line==b"HASBASE64":
+                    print("Remote host supports base64")
+                    mechanism = MECHANISM_BASE64
         else:
             print()
             print("Read timed out")
